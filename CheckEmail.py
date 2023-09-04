3cbd56c3ead6f87d2c4ea71bd7048b3aa314efe2
janmayos <jangasca5@gmail.com>
2023-09-03 19:26:46 -0600

    Evaluador de correos y colores de salida

--- CheckEmail.py
+++ CheckEmail.py
@@ -1,3 +1,11 @@
+import re
+from termcolor import colored
+
+pat = re.compile(r"[a-zA-Z_]{1}[a-zA-Z1-9_-]*@+[a-zA-Z_]+.[a-zA-Z_]+")
+
 with open("correos.txt") as file:
     for linea in file:
-        print(linea)
+        if re.fullmatch(pat, linea.rstrip()):
+            print(colored(f"'{linea.rstrip()}'\tMAIL!", 'green'))
+        else:
+            print(colored(f"'{linea.rstrip()}'\tNO MAIL", 'red'))
